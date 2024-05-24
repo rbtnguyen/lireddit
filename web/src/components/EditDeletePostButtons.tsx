2e473e7269d3be6c763e0e0f17e73352bf4563ac
Binh Nguyen <rbtnguyen@gmail.com>
2024-05-23 22:35:05 -0400

    web

---
+++ web/src/components/EditDeletePostButtons.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Icon, IconButton, Link } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useDeletePostMutation, useMeQuery } from "../generated/graphql";
+
+interface EditDeletePostButtonProps {
+  id: number;
+  creatorId: number;
+}
+
+export const EditDeletePostButton: React.FC<EditDeletePostButtonProps> = ({
+  id,
+  creatorId,
+}) => {
+  const [{ data: meData }] = useMeQuery();
+  const [, deletePost] = useDeletePostMutation();
+
+  if (meData?.me?.id !== creatorId) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <NextLink href="/post/edit/[id]" as={`/post/edit/${id}`}>
+        <IconButton as={Link} mr={4} icon="edit" aria-label="Edit Post" />
+      </NextLink>
+      <IconButton
+        icon="delete"
+        aria-label="Delete Post"
+        onClick={() => {
+          deletePost({ id });
+        }}
+      />
+    </Box>
+  );
+};
